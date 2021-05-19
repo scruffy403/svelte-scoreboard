9e29eb4bc454c802ad03211f4195500934aec349
John <scruffy403@gmail.com>
2021-05-19 17:21:06 +0100

    Implemented add player functionality

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Navbar from './Navbar.svelte';
 	import Player from './Player.svelte';
+	import AddPlayer from './AddPlayer.svelte';
 
 	let players = [
 		{
@@ -17,10 +18,16 @@
 		}
 	];
 
+	const addPlayer = (e) => {
+		const newPlayer = e.detail;
+		players = [...players, newPlayer];
+	}
+
 </script>
 <Navbar/>
 
 <div class="container">
+	<AddPlayer on:addplayer={addPlayer} />
 	{#if players.length === 0}
 		<p>No Players</p>
 	{:else}
